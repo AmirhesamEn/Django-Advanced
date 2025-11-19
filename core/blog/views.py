7bba3e4b7575a180bdcff01675070eb4a3790c7b
AmirhesamEn <webworldtutu@gmail.com>
2025-11-19 10:27:33 +0330

    EP-4-18

---
+++ core/blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+
+from blog.models import Post
+# Create your views here.
+
+class IndexView(TemplateView):
+    '''
+    a class base view to show index page
+    '''
+    template_name = 'blog/test.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] = 'Deven'
+        return context
+    
+class PostListView(ListView):
+    model = Post
+    context_object_name = "posts"
+    template_name = 'blog/list.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        return super().get_queryset()
+    
+    
